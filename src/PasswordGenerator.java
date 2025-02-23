677878f921a8d0651ef39745488e6b641ff78cdb
fabiolunghi <f.lunghi96@gmail.com>
2025-02-23 18:05:19 +0100

    add git.ignore and exercise

--- src/PasswordGenerator.java
+++ src/PasswordGenerator.java
@@ -1,5 +1,31 @@
+
+import java.util.Scanner;
+
 public class PasswordGenerator {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Inserisci il tuo nome");
+        String nome = scan.nextLine();
+
+        System.out.println("Inserisci il tuo cognome");
+        String cognome = scan.nextLine();
+
+        System.out.println("Inserisci il tuo colore preferito");
+        String colorePreferito = scan.nextLine();
+
+        System.out.println("Inserisci il giorno della tua data di nascita");
+        int giornoNascita = scan.nextInt();
+        
+        System.out.println("Inserisci il mese della tua data di nascita");
+        int meseNascita = scan.nextInt();
+
+        System.out.println("Inserisci anno di nascita");
+        int annoNascita = scan.nextInt();
+
+        int totaleNascita = giornoNascita + meseNascita + annoNascita;
+
+
+        System.out.println("La tua password sarà " + nome + "-" + cognome + "-" + colorePreferito + "-" + totaleNascita);
     }
 }
